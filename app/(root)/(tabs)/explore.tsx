8b32c56b0439147c2736560085c321d3338cbb38
Billal Hossain <billalhossain.bhj@gmail.com>
2025-01-25 23:26:59 +0600

    implement tab bars

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Explore = () => {
   return (
-	<View>
-	  <Text>Explore</Text>
-	</View>
-  )
+    <SafeAreaView>
+      <View>
+        <Text>Explore</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 export default Explore
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,27 +1,21 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
-      <Link
-        href={"/sign-in"}
-        className="text-lg bg-primary-300 rounded-lg py-3 px-5 text-white"
+    <SafeAreaView>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        Sign In
-      </Link>
-      <Link href={"/explore"}>Explore</Link>
-      <Link href={"/profile"}>Profile</Link>
-      <Link href={"/properties/1"}>Property</Link>
-    </View>
+        <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
